45ae2786a2c17a57b8aad800af57084ef553116e
Rui Braz <ruijunior2175@gmail.com>
2017-04-12 11:22:11 -0300

    commit 01

---
+++ app/src/androidTest/java/br/com/allmost/meuslivros/ApplicationTest.java
@@ -0,0 +1,11 @@
+package br.com.allmost.meuslivros;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
